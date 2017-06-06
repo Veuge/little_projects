bf0b36cae658c52b84a5d6867289163c177cb6c9
Veuge <nicky.0592@gmail.com>
2017-06-06 18:37:36 -0400

    better looks

--- 2.hn/hn.js
+++ 2.hn/hn.js
@@ -4,6 +4,7 @@ function loadData(initPos, quantity, JSONObject){
     for (var i = initPos; i < initPos + quantity && initPos + quantity < JSONObject.results.length; i++) {
         var nodeLi = document.createElement("li");
         var nodeP = document.createElement("p");
+        nodeP.className = "libname"
         var nodeA = document.createElement("a");
         var nodeName = document.createTextNode(JSONObject.results[i].name);
         var nodeLatest = document.createTextNode(JSONObject.results[i].latest);
@@ -45,7 +46,7 @@ window.onload = (function(){
         console.log("Client Height " + clientHeight);
         console.log("Difference " + (pageHeight - (clientHeight + scrollPos)));
 
-        if (pageHeight - (clientHeight + scrollPos) < 200) {
+        if (pageHeight - (clientHeight + scrollPos) < 50) {
             alert("load!");
             loadData(position, quantity, JSONObject);
             position += quantity;
